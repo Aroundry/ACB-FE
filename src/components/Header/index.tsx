e6f080bf8fdd43d34f30038e985bbb14f9e62d30
parktaewan <xodhks0615@naver.com>
2023-02-27 14:17:04 +0900

    ACB-13 UI 렌더링 Header 작업 진행 중

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,9 @@
+import styled from '@emotion/styled';
+
+export const Header = () => {
+  return <HeaderWrapper></HeaderWrapper>;
+};
+
+const HeaderWrapper = styled.div`
+  background-color: #f2f4f6;
+`;
